471eec37d7b81a0075a9c4dbc11503c31623fdfa
TaeKuenPark <46333025+TaeKuenPark@users.noreply.github.com>
2019-05-13 13:29:53 +0900

    Create Basic.h

---
+++ BaseFrame/Basic.h
@@ -0,0 +1,12 @@
+#pragma once
+#include<stdio.h>
+#include<iostream>
+
+#include<Windows.h>
+#include<tchar.h>
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM IParam);
+HINSTANCE g_hInstance;
+
+LPSTR lpszClass = "First";
+// 파일의 스크린의 이름을 
